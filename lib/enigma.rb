a28502cf42e32d33ccfd6e88558f57b8707d81fe
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-02-26 15:35:42 -0700

    New key method and added attr_reader

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,24 +1,18 @@
 class Enigma
   require 'pry'
-  def initialize
-    # @all_the_keys = Hash.new(0)
-  end
+  attr_reader :key
 
   chars = ("a".."z").to_a << " "
-
-  def key
-    number = [*00000..99999]
-    current_key = number.sample
+  nums = (0..99999).to_a
     # binding.pry
-    current_key.to_s.rjust(5, '0')
-  end
+  key = nums.sample.to_s.rjust(5, '0')
 
-  def key_to_hash
-    key = "12345"
-    # binding.pry
-    arr = key.split("").map { |d| d.to_i }
-    arr.each_slice(1).to_a
-  end
+  # def key_to_hash
+  #   key = "12345"
+  #   arr = key.split("").map { |d| d.to_i }
+  #   arr.each_slice(1).to_a
+  # end
+
   # def encrypt(message, key, date)
   # end
   #
